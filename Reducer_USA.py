6f1464c8b348c5284e43082671046910caab869f
RitishBandi30 <S525701@mail.nwmissouri.edu>
2017-04-18 18:51:47 -0500

    Update Reducer_USA.py

--- Reducer_USA.py
+++ Reducer_USA.py
@@ -31,3 +31,18 @@ if oldSport != None:
 
 sorted.close() 
 results.close()
+
+result = open("reducer_output_USA.txt", "r") 
+
+
+max = 0
+sport = ""
+r = result.readlines()
+for l in r:
+    data = l.strip().split("\t");
+    if float(data[1]) > float(max):
+        max = float(data[1])
+        sport = data[0]
+
+print("USA won {0} gold medals in {1}".format(max,sport))
+result.close()
